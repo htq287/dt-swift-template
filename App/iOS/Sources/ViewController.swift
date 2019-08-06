b134445563e0100089ed98214ff9644968bb223a
Hung Q. Truong <htq287@gmail.com>
2019-08-07 01:53:59 +0700

    * Create default sources for the iOS/macOS

---
+++ App/iOS/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TPLProjectName-iOS
+//
+//  Created by Hung Truong on 8/7/19.
+//  Copyright © 2019 hungtq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ App/macOS/Sources/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TPLProjectName-macOS
+//
+//  Created by Hung Truong on 8/7/19.
+//  Copyright © 2019 hungtq. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
