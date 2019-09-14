a73b981f2a1354fa0b3a01d1733208603e7e7f4f
Hung Q. Truong <htq287@gmail.com>
2019-09-14 18:47:54 +0700

    ### Updates * dependencies * add 'TPLProjectNameCore' target

---
+++ Template/TPLProjectNameCore/TPLProjectNameCore.h
@@ -0,0 +1,19 @@
+//
+//  TPLProjectNameCore.h
+//  TPLProjectNameCore
+//
+//  Created by TPLAuthor on 5/12/19.
+//  Copyright © 2019 TPLOrganizationName. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TPLProjectNameCore.
+FOUNDATION_EXPORT double TPLProjectNameCoreVersionNumber;
+
+//! Project version string for TPLProjectNameCore.
+FOUNDATION_EXPORT const unsigned char TPLProjectNameCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPLProjectNameCore/PublicHeader.h>
+
+
