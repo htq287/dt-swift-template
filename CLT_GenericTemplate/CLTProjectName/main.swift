2468ecbc451c67170a327545eb211f4691ae965f
Hung Q. Truong <htq287@gmail.com>
2019-10-06 23:53:33 +0700

    * add generic template for command line tool

---
+++ CLT_GenericTemplate/CLTProjectName/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CLTProjectName
+//
+//  Created by Hung Truong on 10/6/19.
+//  Copyright © 2019 hungtq. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
